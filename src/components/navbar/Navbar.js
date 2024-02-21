30df3c1211863b88ea8ef56776e73b8f3e0949de
Alessandra F. Lacerda <flacerdaale@gmail.com>
2024-02-20 22:32:12 -0300

    feat: creates hero section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">
-                Portfolio
+                Ale Lacerda
                 </a>
             <div className={styles.menu}>
                 <img 
