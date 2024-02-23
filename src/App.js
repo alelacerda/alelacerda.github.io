e1b8ab326abb8d57b15bed71f2ca3b9137f17a8d
Alessandra F. Lacerda <flacerdaale@gmail.com>
2024-02-23 15:00:24 -0300

    feat: updates main page and background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import styles from './App.module.css';
 import { Navbar } from './components/navbar/Navbar';
 import { Hero } from './components/hero/Hero';
 import { About } from './components/about/About';
+import { Experience } from './components/experience/Experience';
+import { Skills } from './components/skills/Skills';
+import { Education } from './components/education/Education';
+import { Awards } from './components/awards/Awards';
+import { Contact } from './components/contact/Contact';
 
 function App() {
   return (
@@ -9,6 +14,14 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
+      <Experience />
+      <Awards />
+      <Education />
+      <Contact />
+      
+      <div className={styles.topBlur}/>
+      <div className={styles.bottomBlur}/>
     </div>
   );
 }
